855bde2c96e33f41ff53f31ac6f2b50eeeddfdee
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-21 13:54:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ useEffect(() => {
   const getRecipe= async()=>{
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`); 
     const data= await response.json();
-console.log(data.hits)
 setMyRecipes(data.hits);
   } 
   
