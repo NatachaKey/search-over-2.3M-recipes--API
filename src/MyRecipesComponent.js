c60296ae965b2145f711a42379eff9514b697347
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-25 21:01:00 +0200

    Update MyRecipesComponent.js

--- src/MyRecipesComponent.js
+++ src/MyRecipesComponent.js
@@ -1,18 +1,18 @@
 import './App.css';
 
-function MyRecipesComponent({label, cuisineType, image, calories, ingredients, mealType, url}){
+function MyRecipesComponent({ label, cuisineType, image, calories, ingredients, mealType, url }){
     return(
         <div>
-            <h2>{label}</h2>
-            <p className="grey"> {cuisineType} cuisine </p>
-            <img className="foto" src={image} alt="dish" />
-            <p>{calories.toFixed()} kcal</p>
-            <ul>{ingredients.map(element =>(
-                <li> {element}</li>
+            <h2>{ label }</h2>
+            <p className="grey"> { cuisineType } cuisine </p>
+            <img className="foto" src={ image } alt="dish" />
+            <p>{ calories.toFixed() } kcal</p>
+            <ul>{ ingredients.map(element =>(
+                <li> { element }</li>
             ))}
             </ul> 
-            <p className='green'>Meal type: {mealType} </p>
-            <p className='red'>Preparation: <a  className='link' href={url}>here </a> </p>
+            <p className='green'>Meal type: { mealType } </p>
+            <p className='red'>Preparation: <a  className='link' href={ url }>here </a> </p>
         </div>
     )
 }
