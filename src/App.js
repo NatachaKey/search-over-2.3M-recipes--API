6b1ec83bcac66c2f51c768b7dbb22d672e4f8db8
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-28 12:14:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,6 +54,8 @@ return(
   </div>
 
 <form onSubmit={ finalSearch }>
+    
+ window.location.reload(false);
 
 <input className='search' placeholder='Search by ingredients...' onChange={ myRecipeSearch } value={ mySearch } /> <svg className='searchicon'  onClick={ finalSearch } xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 16 16">
     <path d="M6.5 13a6.474 6.474 0 0 0 3.845-1.258h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.008 1.008 0 0 0-.115-.1A6.471 6.471 0 0 0 13 6.5 6.502 6.502 0 0 0 6.5 0a6.5 6.5 0 1 0 0 13Zm0-8.518c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"/>
@@ -63,10 +65,16 @@ return(
 
 <div className='column'>
  
-{ myRecipes.length <0 ? <p> Cannot find it </p>} :
+if (myRecipes.length <0) {
+ window.location.reload(false);
+ <p> Cannot find it </p>
+} 
+
+else{
  
 {myRecipes.map((element, index) => (
-  <MyRecipesComponent key={index}
+  <MyRecipesComponent 
+    key={index}
     label={element.recipe.label}
     cuisineType = {element.recipe.cuisineType[0]}
     image={element.recipe.image} 
@@ -75,7 +83,6 @@ return(
     mealType={element.recipe.mealType[0]}
     url={element.recipe.url}/>
 ))}
-
 }
 
 </div>
